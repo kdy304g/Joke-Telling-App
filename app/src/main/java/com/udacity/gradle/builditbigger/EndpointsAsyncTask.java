b67da0d1e8b8c6631edbe4f7d73f303120d0bb7b
Danny Kang <kdy304g@gmail.com>
2020-06-22 20:38:45 +0900

    remove unnecessary manifest from paid and main and added strings value for free flavor

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -44,7 +44,7 @@ class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
         try {
             return myApiService.tellJoke().execute().getData();
         } catch (IOException e) {
-            return e.getMessage();
+            return "";
         }
     }
 
